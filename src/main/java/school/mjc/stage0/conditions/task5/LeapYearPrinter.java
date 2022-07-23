bb60e407b420ba2e1e145bc2fce24abec8764560
NANO <File changed>
2022-07-24 00:18:05 +0600

    2 submission

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -3,7 +3,8 @@ package school.mjc.stage0.conditions.task5;
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
         switch (year){
-            case(1):
+            case (2000):
+            case (2004):
             System.out.println("leap");
             break;
             default:
--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -4,34 +4,25 @@ public class RangeNumbersPrinter {
     public void printRange(int number) {
         switch (number){
             case (1):
-                System.out.println("1");
-                break;
-            case (2):
-                System.out.println("2");
-                break;
+                case (2):
+
             case (3):
-                System.out.println("3");
-                break;
+
             case(4):
-                System.out.println("4");
-                break;
+
             case (5):
-                System.out.println("5");
+                System.out.println("number is between 1 and 5");
                 break;
             case (6):
-                System.out.println("6");
-                break;
+
             case (7):
-                System.out.println("7");
-                break;
+
             case (8):
-                System.out.println("8");
-                break;
+
             case (9):
-                System.out.println("9");
-                break;
+
             case (10):
-                System.out.println("10");
+                System.out.println("number is between 6 and 10");
                 break;
             default:
                 System.out.println("out of range");
