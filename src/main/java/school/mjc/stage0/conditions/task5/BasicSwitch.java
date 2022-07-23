c192e3900f7f9530bd1be7f17bd7033b3b75d1e4
NANO <File changed>
2022-07-24 00:34:08 +0600

    4 submission

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -2,11 +2,9 @@ package school.mjc.stage0.conditions.task5;
 
 public class BasicSwitch {
     public static void main(String[] args) {
-        int a = 20;
+        int a = 21;
         switch (a) {
-            default:
-                System.out.println("default");
-                break;
+
             case 7:
                 System.out.println("seven");
                 break;
@@ -16,7 +14,9 @@ public class BasicSwitch {
             case 20:
                 System.out.println("twenty");
                 break;
-
+            default:
+                System.out.println("default");
+                break;
         }
     }
 }
