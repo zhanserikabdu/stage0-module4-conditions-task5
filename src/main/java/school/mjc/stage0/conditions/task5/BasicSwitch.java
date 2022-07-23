7652630ffc1ef404184de470080ae8e3f7b42a45
NANO <File changed>
2022-07-24 00:28:42 +0600

    3 submission

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -4,7 +4,9 @@ public class BasicSwitch {
     public static void main(String[] args) {
         int a = 20;
         switch (a) {
-
+            default:
+                System.out.println("default");
+                break;
             case 7:
                 System.out.println("seven");
                 break;
@@ -14,9 +16,7 @@ public class BasicSwitch {
             case 20:
                 System.out.println("twenty");
                 break;
-            default:
-                System.out.println("default");
-                break;
+
         }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,14 +2,13 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
-        int Feb = 28;
-        int June = 30;
+
         switch (month){
 
-            case (28):
+            case (2):
                 System.out.println("28");
                 break;
-            case (30):
+            case (6):
                 System.out.println("30");
                 break;
             default:
